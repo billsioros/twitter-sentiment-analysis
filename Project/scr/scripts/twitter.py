1cecac7201a4e8fceb7b0c05542d40df11d0961a
Qwerkykk <thebeaterkk@hotmail.com>
2019-05-11 19:16:27 +0300

    RR-final

--- Project/scr/scripts/twitter.py
+++ Project/scr/scripts/twitter.py
@@ -13,10 +13,39 @@ if __name__ == "__main__":
 
     for method in ['word-2-vec']:
         knownVectors = Vectorizer(method).vectorize(preprocessor)
-
+    
     preprocessor1 = Preprocessor('../test2017.tsv', Cruncher())
 
     for method in ['word-2-vec']:
         unknownVectors = Vectorizer(method).vectorize(preprocessor1)
 
-    RR.roundRobin(preprocessor.tweets,knownVectors, unknownVectors)
+    
+    #print(knownVectors.keys())
+    
+    #print(preprocessor.labels)
+    
+    #for key in knownVectors.keys():
+       # print(preprocessor.labels[key])
+
+    #for key in unknownVectors.keys():
+        #print(preprocessor1.labels[key])
+
+    #print(preprocessor.labels)
+    
+    #print(unknownVectors.keys())
+    #print(preprocessor1.labels)
+    #print(preprocessor.by_label(['positive']))
+
+
+    #print(knownVectors.keys())
+
+    pos = [key for key in knownVectors.keys() if preprocessor.labels[key] == 'positive']
+    
+    #for key in pos:
+        #print(knownVectors[key])
+
+    
+
+
+    RR.roundRobin(preprocessor.labels,knownVectors, unknownVectors)
+
