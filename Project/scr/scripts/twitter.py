ff8054a5731afe05aa0ccee85432fa94585e541b
billsioros <billsioros97@gmail.com>
2019-05-12 03:28:02 +0300

    Updated twitter main

--- Project/scr/scripts/twitter.py
+++ Project/scr/scripts/twitter.py
@@ -3,27 +3,71 @@ from cruncher import Cruncher
 from preprocessor import Preprocessor
 from visualizer import Visualizer
 from vectorizer import Vectorizer
+from classifier import Classifier
+from evaluator import Evaluator
+from dictionary import Dictioanry
 
-import roundRobin as RR
+import time
+
+def visualization(train_filename, cruncher_type='lemmatizer'):
+
+    preprocessor = Preprocessor(train_filename, Cruncher(cruncher_type))
+
+    return Visualizer(preprocessor)
+
+def evaluation(train_filename, test_filename, dictionary_root='..\\..\\lexica', cruncher_type='lemmatizer', vectorizer_type='word2vec'):
+
+    beg = time.time()
+
+    try:
+
+        vectorizer = Vectorizer(vectorizer_type)
+
+        _, train_vectors = vectorizer.vectorize(train_filename, dictionary_root)
+
+    except:
+
+        cruncher = Cruncher(cruncher_type)
+
+        train_preprocessor = Preprocessor(train_filename, cruncher)
+
+        dictionary = Dictioanry(dictionary_root) if dictionary_root else None
+
+        train_vectors = Vectorizer(vectorizer_type).vectorize(train_preprocessor, dictionary)
+
+        train_vectors = list(train_vectors.values())
+
+
+    test_preprocessor  = Preprocessor(test_filename, cruncher)
+
+    test_vectors  = vectorizer.vectorize(test_preprocessor, dictionary)
+
+    test_ids, test_vectors = list(test_vectors.keys()), list(test_vectors.values())
+
+    train_labels = list(train_preprocessor.labels.values())
+
+    evaluator = Evaluator()
+
+    for classifing in ['knn', 'svm']:
+
+        classifier = Classifier(train_vectors, train_labels, classifing)
+
+        predictions = classifier.predict(list(test_vectors))
+
+        for metric in ['f1-score', 'accuracy-score']:
+
+            value = evaluator.evaluate(dict(zip(test_ids, predictions)), metric)
+
+            print('<LOG>: The accuracy of', "'" + metric + "'", 'according to the', "'" + metric + "'", 'is', '{0:.6f}'.format(value))
+
+    end = time.time()
+
+    print('\n\nElapsed time:', '{0:.6f}'.format(end - beg), 'seconds')
+
 
 if __name__ == "__main__":
 
-    preprocessor = Preprocessor('../train.tsv', Cruncher())
+    # '..\\..\\twitter_data\\train2017.tsv'
 
-    #visualization = Visualizer(preprocessor).visualize()
-
-    for method in ['word-2-vec']:
-        knownVectors = Vectorizer(method).vectorize(preprocessor)
-    
-    preprocessor1 = Preprocessor('../test2017.tsv', Cruncher())
-
-
-    for method in ['word-2-vec']:
-        unknownVectors = Vectorizer(method).vectorize(preprocessor1)
-
-    RR.roundRobin(preprocessor.labels,knownVectors, unknownVectors)
-
-    model = Vectorizer(preprocessor).vectorize(labels = ['positive'])
-    
-    Visualizer(preprocessor).tsne(model)
+    evaluation('train.tsv', 'test.tsv', vectorizer_type='bagofwords')
 
